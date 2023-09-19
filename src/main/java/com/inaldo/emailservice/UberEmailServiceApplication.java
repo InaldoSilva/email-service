cc2b6f314e7d433b79ce00faa671c9d4f22522b6
Inaldo Silva <104039452+InaldoSilva@users.noreply.github.com>
2023-09-18 21:26:01 -0300

    Add files via upload

---
+++ src/main/java/com/inaldo/emailservice/UberEmailServiceApplication.java
@@ -0,0 +1,15 @@
+package com.inaldo.emailservice;
+//E-mail App
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberEmailServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(UberEmailServiceApplication.class, args);
+	}
+
+}
+
